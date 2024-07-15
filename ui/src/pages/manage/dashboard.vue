eb00424617bad4d9974e5e0daaa31396ac8381b7
autoo <autoopm@admin.com>
2024-07-16 00:41:15 +0800

    feat：页面微调

--- ui/src/pages/manage/dashboard.vue
+++ ui/src/pages/manage/dashboard.vue
@@ -650,6 +650,13 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.col-span-2{
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+}
 .todo {
   margin-top: 0px;
   padding: 0 12px;
