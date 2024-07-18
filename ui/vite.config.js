f0999c797d69f3e36c7cc4842c11a9c8e1d1f1a9
autoo <autoopm@admin.com>
2024-07-18 17:49:43 +0800

    fix：更新最新的sql

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -69,7 +69,7 @@ export default defineConfig({
     open: true,
     proxy: {
       "/api/": {
-        target: "http://localhost:8083",
+        target: "http://localhost:8081",
         changeOrigin: true,
         //rewrite: (path) => path.replace(/^\/request/, ""),
       },
